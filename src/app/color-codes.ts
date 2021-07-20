48c68644f51d72799dd6bdc40919e606e82b1acc
Mike Brown <MBrown3@paylocity.com>
2021-07-20 15:42:55 -0600

    Making the top part tied to a model

---
+++ src/app/color-codes.ts
@@ -0,0 +1,9 @@
+export class ColorCodes {
+  constructor(
+    public digit1: string,
+    public digit2: string,
+    public digit3: string,
+    public multiplier: string,
+    public tolerance: string
+  ) {  }
+}
---
+++ src/app/values.ts
@@ -0,0 +1,9 @@
+export class Values {
+  constructor(
+    public digit1: number,
+    public digit2: number,
+    public digit3: number,
+    public multiplier: number,
+    public tolerance: number
+  ) {  }
+}
