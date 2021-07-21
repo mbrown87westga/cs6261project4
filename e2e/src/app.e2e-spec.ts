2b3e2f13116d2ba49830865b10b6438acf0c4d92
Mike Brown <MBrown3@paylocity.com>
2021-07-20 21:08:44 -0600

    checking the colors of the debug panel as well

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -21,6 +21,11 @@ describe('workspace-project App', () => {
     page.setMultiplier('blue');
     page.setTolerance('violet');
     expect(page.getResults()).toEqual('865M +/- 0.1%');
+    expect(page.getDigit1ColorName()).toEqual('grey');
+    expect(page.getDigit2ColorName()).toEqual('blue');
+    expect(page.getDigit3ColorName()).toEqual('green');
+    expect(page.getMultiplierColorName()).toEqual('blue');
+    expect(page.getToleranceColorName()).toEqual('violet');
   });
 
   it('should set result value correctly when set to minimum value possible', () => {
@@ -31,6 +36,11 @@ describe('workspace-project App', () => {
     page.setMultiplier('silver');
     page.setTolerance('grey');
     expect(page.getResults()).toEqual('0 +/- 0.05%');
+    expect(page.getDigit1ColorName()).toEqual('black');
+    expect(page.getDigit2ColorName()).toEqual('black');
+    expect(page.getDigit3ColorName()).toEqual('black');
+    expect(page.getMultiplierColorName()).toEqual('silver');
+    expect(page.getToleranceColorName()).toEqual('grey');
   });
 
   it('should set result value correctly when set to minimum non zero value possible', () => {
@@ -41,6 +51,11 @@ describe('workspace-project App', () => {
     page.setMultiplier('silver');
     page.setTolerance('grey');
     expect(page.getResults()).toEqual('0.01 +/- 0.05%');
+    expect(page.getDigit1ColorName()).toEqual('black');
+    expect(page.getDigit2ColorName()).toEqual('black');
+    expect(page.getDigit3ColorName()).toEqual('brown');
+    expect(page.getMultiplierColorName()).toEqual('silver');
+    expect(page.getToleranceColorName()).toEqual('grey');
   });
 
   it('should set result value correctly when set to maximum value possible', () => {
@@ -51,16 +66,11 @@ describe('workspace-project App', () => {
     page.setMultiplier('white');
     page.setTolerance('none');
     expect(page.getResults()).toEqual('999G +/- 20%');
-  });
-
-  it('should set result value correctly when set to maximum value possible', () => {
-    page.navigateTo();
-    page.setDigit1('white');
-    page.setDigit2('white');
-    page.setDigit3('white');
-    page.setMultiplier('white');
-    page.setTolerance('none');
-    expect(page.getResults()).toEqual('999G +/- 20%');
+    expect(page.getDigit1ColorName()).toEqual('white');
+    expect(page.getDigit2ColorName()).toEqual('white');
+    expect(page.getDigit3ColorName()).toEqual('white');
+    expect(page.getMultiplierColorName()).toEqual('white');
+    expect(page.getToleranceColorName()).toEqual('none');
   });
 
   it('should set result value correctly when set to one possible encoding of 1K', () => {
@@ -71,6 +81,11 @@ describe('workspace-project App', () => {
     page.setMultiplier('orange');
     page.setTolerance('brown');
     expect(page.getResults()).toEqual('1K +/- 1%');
+    expect(page.getDigit1ColorName()).toEqual('black');
+    expect(page.getDigit2ColorName()).toEqual('black');
+    expect(page.getDigit3ColorName()).toEqual('brown');
+    expect(page.getMultiplierColorName()).toEqual('orange');
+    expect(page.getToleranceColorName()).toEqual('brown');
   });
 
   it('should set result value correctly when set to another possible encoding of 1K', () => {
@@ -81,6 +96,11 @@ describe('workspace-project App', () => {
     page.setMultiplier('red');
     page.setTolerance('red');
     expect(page.getResults()).toEqual('1K +/- 2%');
+    expect(page.getDigit1ColorName()).toEqual('black');
+    expect(page.getDigit2ColorName()).toEqual('brown');
+    expect(page.getDigit3ColorName()).toEqual('black');
+    expect(page.getMultiplierColorName()).toEqual('red');
+    expect(page.getToleranceColorName()).toEqual('red');
   });
 
   it('should set result value correctly when set to yet another possible encoding of 1K', () => {
@@ -91,6 +111,11 @@ describe('workspace-project App', () => {
     page.setMultiplier('brown');
     page.setTolerance('gold');
     expect(page.getResults()).toEqual('1K +/- 5%');
+    expect(page.getDigit1ColorName()).toEqual('brown');
+    expect(page.getDigit2ColorName()).toEqual('black');
+    expect(page.getDigit3ColorName()).toEqual('black');
+    expect(page.getMultiplierColorName()).toEqual('brown');
+    expect(page.getToleranceColorName()).toEqual('gold');
   });
 
   afterEach(async () => {
