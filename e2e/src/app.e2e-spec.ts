960b523817334737d50234fbc76e7e66abb91e4b
Mike Brown <MBrown3@paylocity.com>
2021-07-20 20:55:38 -0600

    adding some e2e tests - testing value from assignment, minimum, and maximum values

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -13,6 +13,56 @@ describe('workspace-project App', () => {
     expect(page.getTitleText()).toEqual('Welcome to Resistance Calculator!');
   });
 
+  it('should set result value correctly when set to sample values from assignment', () => {
+    page.navigateTo();
+    page.setDigit1('grey');
+    page.setDigit2('blue');
+    page.setDigit3('green');
+    page.setMultiplier('blue');
+    page.setTolerance('violet');
+    expect(page.getResults()).toEqual('865M +/- 0.1%');
+  });
+
+  it('should set result value correctly when set to minimum value possible', () => {
+    page.navigateTo();
+    page.setDigit1('black');
+    page.setDigit2('black');
+    page.setDigit3('black');
+    page.setMultiplier('silver');
+    page.setTolerance('grey');
+    expect(page.getResults()).toEqual('0 +/- 0.05%');
+  });
+
+  it('should set result value correctly when set to minimum non zero value possible', () => {
+    page.navigateTo();
+    page.setDigit1('black');
+    page.setDigit2('black');
+    page.setDigit3('brown');
+    page.setMultiplier('silver');
+    page.setTolerance('grey');
+    expect(page.getResults()).toEqual('0.01 +/- 0.05%');
+  });
+
+  it('should set result value correctly when set to maximum value possible', () => {
+    page.navigateTo();
+    page.setDigit1('white');
+    page.setDigit2('white');
+    page.setDigit3('white');
+    page.setMultiplier('white');
+    page.setTolerance('none');
+    expect(page.getResults()).toEqual('999G +/- 20%');
+  });
+
+  it('should set result value correctly when set to maximum value possible', () => {
+    page.navigateTo();
+    page.setDigit1('white');
+    page.setDigit2('white');
+    page.setDigit3('white');
+    page.setMultiplier('white');
+    page.setTolerance('none');
+    expect(page.getResults()).toEqual('999G +/- 20%');
+  });
+
   afterEach(async () => {
   });
 });
--- e2e/src/app.po.ts
+++ e2e/src/app.po.ts
@@ -8,4 +8,17 @@ export class AppPage {
   getTitleText(): Promise<string> {
     return element(by.css('app-root .content h1')).getText() as Promise<string>;
   }
+
+  getToleranceColorName(): Promise<string> { return element(by.id('toleranceColorName')).getText() as Promise<string>; }
+  getMultiplierColorName(): Promise<string> { return element(by.id('multiplierColorName')).getText() as Promise<string>; }
+  getDigit3ColorName(): Promise<string> { return element(by.id('digit3ColorName')).getText() as Promise<string>; }
+  getDigit2ColorName(): Promise<string> { return element(by.id('digit2ColorName')).getText() as Promise<string>; }
+  getDigit1ColorName(): Promise<string> { return element(by.id('digit1ColorName')).getText() as Promise<string>; }
+  getResults  (): Promise<string> { return element(by.id('results')).getText() as Promise<string>; }
+  
+  setTolerance(val): void { element(by.id('tolerance')).element(by.cssContainingText('option', val)).click(); }
+  setMultiplier(val): void { element(by.id('multiplier')).element(by.cssContainingText('option', val)).click(); }
+  setDigit3(val): void { element(by.id('digit3')).element(by.cssContainingText('option', val)).click(); }
+  setDigit2(val): void { element(by.id('digit2')).element(by.cssContainingText('option', val)).click(); }
+  setDigit1(val): void { element(by.id('digit1')).element(by.cssContainingText('option', val)).click(); }
 }
