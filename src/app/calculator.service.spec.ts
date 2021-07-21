c4561b7d4cbedee5933c0fd99996d6232fa6c676
Mike Brown <MBrown3@paylocity.com>
2021-07-20 18:29:53 -0600

    adding two new tests, and undoing accidental changes to the package.json and packagelock.json

--- src/app/calculator.service.spec.ts
+++ src/app/calculator.service.spec.ts
@@ -19,4 +19,33 @@ describe('CalculatorService', () => {
       {value: 9, name: 'white (9)', color: 'white'},
     ]);
   });
+  it('multipliers should be properly populated', () => {
+    expect(service.multipliers).toEqual([
+      {value: 0.01, name: 'silver (x0.01)', color: 'silver'},
+      {value: 0.1, name: 'gold (x0.1)', color: 'gold'},
+      {value: 1, name: 'black (x1)', color: 'black'},
+      {value: 10, name: 'brown (x10)', color: 'brown'},
+      {value: 100, name: 'red (x100)', color: 'red'},
+      {value: 1000, name: 'orange (x1K)', color: 'orange'},
+      {value: 10000, name: 'yellow (x10K)', color: 'yellow'},
+      {value: 100000, name: 'green (x100K)', color: 'green'},
+      {value: 1000000, name: 'blue (x1M)', color: 'blue'},
+      {value: 10000000, name: 'violet (x10M)', color: 'violet'},
+      {value: 100000000, name: 'grey (x100M)', color: 'grey'},
+      {value: 1000000000, name: 'white (x1G)', color: 'white'},
+    ]);
+  });
+  it('tolerances should be properly populated', () => {
+    expect(service.tolerances).toEqual([
+      {value: 0.05, name: 'grey (0.05%)', color: 'grey'},
+      {value: 0.1, name: 'violet (0.1%)', color: 'violet'},
+      {value: 0.25, name: 'blue (0.25%)', color: 'blue'},
+      {value: 0.5, name: 'green (0.5%)', color: 'green'},
+      {value: 1, name: 'brown (1%)', color: 'brown'},
+      {value: 2, name: 'red (2%)', color: 'red'},
+      {value: 5, name: 'gold (5%)', color: 'gold'},
+      {value: 10, name: 'silver (10%)', color: 'silver'},
+      {value: 20, name: 'none (20%)', color: 'none'},
+    ]);
+  });
 });
